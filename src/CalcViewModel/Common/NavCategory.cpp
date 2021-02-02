ShaunRoselt/calculator

commit 19c2773db9939da599c458de6bedf94b93b9c5b4

switch to use policy manager apis for detecting graphing policy (#1426)
* switch to use policy manager apis for detecting graphing policy

* Remove RegGetValueW from NavCategory.cpp since it is no longer used.

src/CalcViewModel/Common/NavCategory.cpp
@@ -9,10 +9,13 @@
 
 using namespace CalculatorApp;
 using namespace CalculatorApp::Common;
+using namespace Concurrency;
 using namespace Platform;
 using namespace Platform::Collections;
 using namespace std;
 using namespace Windows::Foundation::Collections;
+using namespace Windows::Management::Policies;
+using namespace Windows::System;
 
 namespace UCM = UnitConversionManager;
 
@@ -50,22 +53,6 @@ wchar_t* towchar_t(int number)
     return _wcsdup(wstr.c_str());
 }
 
-extern "C"
-{
-    WINADVAPI LSTATUS APIENTRY RegGetValueW(
-        _In_ HKEY hkey,
-        _In_opt_ LPCWSTR lpSubKey,
-        _In_opt_ LPCWSTR lpValue,
-        _In_ DWORD dwFlags,
-        _Out_opt_ LPDWORD pdwType,
-        _When_(
-            (dwFlags & 0x7F) == RRF_RT_REG_SZ || (dwFlags & 0x7F) == RRF_RT_REG_EXPAND_SZ || (dwFlags & 0x7F) == (RRF_RT_REG_SZ | RRF_RT_REG_EXPAND_SZ)
-                || *pdwType == REG_SZ || *pdwType == REG_EXPAND_SZ,
-            _Post_z_) _When_((dwFlags & 0x7F) == RRF_RT_REG_MULTI_SZ || *pdwType == REG_MULTI_SZ, _Post_ _NullNull_terminated_)
-            _Out_writes_bytes_to_opt_(*pcbData, *pcbData) PVOID pvData,
-        _Inout_opt_ LPDWORD pcbData);
-}
-
 bool IsGraphingModeAvailable()
 {
     static bool supportGraph = Windows::Foundation::Metadata::ApiInformation::IsMethodPresent("Windows.UI.Text.RichEditTextDocument", "GetMath");
@@ -85,25 +72,12 @@ bool IsGraphingModeEnabled()
         return _isGraphingModeEnabledCached->Value;
     }
 
-    DWORD allowGraphingCalculator{ 0 };
-    DWORD bufferSize{ sizeof(allowGraphingCalculator) };
-    // Make sure to call RegGetValueW only on Windows 10 1903+
-    if (RegGetValueW(
-            HKEY_CURRENT_USER,
-            L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Calculator",
-            L"AllowGraphingCalculator",
-            RRF_RT_DWORD, // RRF_RT_DWORD == RRF_RT_REG_DWORD | RRF_RT_REG_BINARY
-            nullptr,
-            reinterpret_cast<LPBYTE>(&allowGraphingCalculator),
-            &bufferSize)
-        == ERROR_SUCCESS)
-    {
-        _isGraphingModeEnabledCached = allowGraphingCalculator != 0;
-    }
-    else
-    {
-        _isGraphingModeEnabledCached = true;
-    }
+    User ^ firstUser;
+    create_task(User::FindAllAsync(UserType::LocalUser)).then([&firstUser](IVectorView<User ^> ^ users) {
+        firstUser = users->GetAt(0); }).wait();
+        auto namedPolicyData = NamedPolicy::GetPolicyFromPathForUser(firstUser, L"Education", L"AllowGraphingCalculator");
+        _isGraphingModeEnabledCached = namedPolicyData->GetBoolean() == true;
+
     return _isGraphingModeEnabledCached->Value;
 }
 

src/CalcViewModel/pch.h
@@ -41,6 +41,7 @@
 #include "winrt/Windows.System.UserProfile.h"
 #include "winrt/Windows.UI.Xaml.h"
 #include "winrt/Windows.Foundation.Metadata.h"
+#include "winrt/Windows.Management.Policies.h"
 
 // The following namespaces exist as a convenience to resolve
 // ambiguity for Windows types in the Windows::UI::Xaml::Automation::Peers

commit dc436c633498a2792f4ed61ab39d37d1667bd1c0

Fixed a graphing calculator "permissions" bug caused by PR #1426 (#1471)
## Fixes a bug caused by https://github.com/microsoft/calculator/pull/1426#.


### Description of the changes:
- The PR #1426 can cause a crash when no users are returned via `User::FindAllAsync(UserType::LocalUser)` when subsequently trying to access the first user. The existing code also does not guarantee that the returned user is the currently active user.
- This fix retrieves the user that opened the app and passes this user into a function to check if this user has the proper permissions to access the graphing mode. This makes sense since the active user is indistinguishable (at least from the app's perspective) to the user who opened the app. This user's permissions are then propagated downwards to properly set up the navigation menu of the app.
- Implementation detail worth pointing out: `s_categoryManifest` is what is used to populate the navigation menu of the app, but this variable is static by design, so a separate function was written to override the appropriate `isEnabled` value in `s_categoryManifest`. This function is called by `onLaunched`.

### How changes were validated:
- Manual testing

src/CalcViewModel/Common/NavCategory.cpp
@@ -60,7 +60,7 @@ bool IsGraphingModeAvailable()
 }
 
 Box<bool> ^ _isGraphingModeEnabledCached = nullptr;
-bool IsGraphingModeEnabled()
+bool IsGraphingModeEnabled(User ^ currentUser = nullptr)
 {
     if (!IsGraphingModeAvailable())
     {
@@ -72,17 +72,19 @@ bool IsGraphingModeEnabled()
         return _isGraphingModeEnabledCached->Value;
     }
 
-    User ^ firstUser;
-    create_task(User::FindAllAsync(UserType::LocalUser)).then([&firstUser](IVectorView<User ^> ^ users) {
-        firstUser = users->GetAt(0); }).wait();
-        auto namedPolicyData = NamedPolicy::GetPolicyFromPathForUser(firstUser, L"Education", L"AllowGraphingCalculator");
-        _isGraphingModeEnabledCached = namedPolicyData->GetBoolean() == true;
+    if (!currentUser)
+    {
+        return true;
+    }
+
+    auto namedPolicyData = NamedPolicy::GetPolicyFromPathForUser(currentUser, L"Education", L"AllowGraphingCalculator");
+    _isGraphingModeEnabledCached = namedPolicyData->GetBoolean() == true;
 
     return _isGraphingModeEnabledCached->Value;
 }
 
 // The order of items in this list determines the order of items in the menu.
-static const list<NavCategoryInitializer> s_categoryManifest = [] {
+static list<NavCategoryInitializer> s_categoryManifest = [] {
     auto res = list<NavCategoryInitializer>{ NavCategoryInitializer{ ViewMode::Standard,
                                                                      STANDARD_ID,
                                                                      L"Standard",
@@ -108,7 +110,7 @@ static const list<NavCategoryInitializer> s_categoryManifest = [] {
     bool supportGraphingCalculator = IsGraphingModeAvailable();
     if (supportGraphingCalculator)
     {
-        const bool isEnabled = IsGraphingModeEnabled();
+        bool isEnabled = IsGraphingModeEnabled();
         res.push_back(NavCategoryInitializer{ ViewMode::Graphing,
                                               GRAPHING_ID,
                                               L"Graphing",
@@ -275,6 +277,25 @@ static const list<NavCategoryInitializer> s_categoryManifest = [] {
                                   true } });
     return res;
 }();
+
+void NavCategory::InitializeCategoryManifest(User ^ user)
+{
+    int i = 0;
+    for (NavCategoryInitializer category : s_categoryManifest)
+    {
+        if (category.viewMode == ViewMode::Graphing)
+        {
+            auto navCatInit = s_categoryManifest.begin();
+            std::advance(navCatInit, i);
+            (*navCatInit).isEnabled = IsGraphingModeEnabled(user);
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
+ }
 
 // This function should only be used when storing the mode to app data.
 int NavCategory::Serialize(ViewMode mode)

src/CalcViewModel/Common/NavCategory.h
@@ -92,7 +92,7 @@ namespace CalculatorApp
             const MyVirtualKey virtualKey;
             const wchar_t* const accessKey;
             const bool supportsNegative;
-            const bool isEnabled;
+            bool isEnabled;
         };
 
     private
@@ -139,6 +139,8 @@ namespace CalculatorApp
             static bool IsGraphingCalculatorViewMode(ViewMode mode);
             static bool IsDateCalculatorViewMode(ViewMode mode);
             static bool IsConverterViewMode(ViewMode mode);
+
+            static void InitializeCategoryManifest(Windows::System::User ^ user);
 
             static Platform::String ^ GetFriendlyName(ViewMode mode);
             static Platform::String ^ GetNameResourceKey(ViewMode mode);

src/Calculator/App.xaml.cpp
@@ -198,12 +198,12 @@ void App::OnLaunched(LaunchActivatedEventArgs ^ args)
         // If the app got pre-launch activated, then save that state in a flag
         m_preLaunched = true;
     }
+    NavCategory::InitializeCategoryManifest(args->User);
     OnAppLaunch(args, args->Arguments);
 }
 
 void App::OnAppLaunch(IActivatedEventArgs ^ args, String ^ argument)
 {
-
     // Uncomment the following lines to display frame-rate and per-frame CPU usage info.
     //#if _DEBUG
     //    if (IsDebuggerPresent())
bee2e7e0ea19bd48db22b29ee86de291068eba5b
Shaun Roselt <Me@ShaunRoselt.com>
2026-04-06 21:14:31 +0200

    feat(history): add history panel test and clear history functionality in programmer mode

--- src/CalculatorUITestFramework/ProgrammerCalculatorPage.cs
+++ src/CalculatorUITestFramework/ProgrammerCalculatorPage.cs
@@ -48,6 +48,8 @@ namespace CalculatorUITestFramework
             {
                 MemoryPanel.NumberpadMCButton.Click();
             }
+
+            HistoryPanel.ClearHistory();
         }
     }
 }
